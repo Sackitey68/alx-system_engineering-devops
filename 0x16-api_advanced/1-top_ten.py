7e4948923eddad791e1a9485ac4aa27c6cec34fc
Sackitey68 <sackitey68@gmail.com>
2023-11-07 07:41:56 -0800

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,46 +1,22 @@
 #!/usr/bin/python3
-""" querry reddit api for subreddit info"""
+"""Contains top_ten function"""
 import requests
-import requests.auth
 
 
 def top_ten(subreddit):
-    """ querry reddit api"""
-    sub = subreddit
-    subreddit = "/r/{}/hot".format(sub)
-    temp = "HolbertonPass845"
-
-    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
-    client_id = "hy4KvoK0W2iDvw"
-    usr_name = "jgadelugo"
-
-    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
-    post_data = {"grant_type": "password",
-                 "username": usr_name,
-                 "password": temp}
-
-    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
-    response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             auth=client_auth, data=post_data, headers=headers)
-    auth_json = response.json()
-
-    token_type = auth_json['token_type']
-    access_token = auth_json['access_token']
-
-    headers = {"Authorization": "{} {}".format(token_type, access_token),
-               "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
-
-    param = {"limit": 10}
-
-    query = "https://oauth.reddit.com{}".format(subreddit)
-    res = requests.get(query, headers=headers, params=param)
-
-    status = res.status_code
-
-    if (status != 200):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
-    else:
-        data = res.json()
-        posts = data["data"]['children']
-        for post in posts:
-            print(post['data']['title'])
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
