14440c4b91101ea6baeb1309b2b6c86ffc2b3b3a
Sackitey68 <sackitey68@gmail.com>
2023-11-08 10:25:02 -0800

    Solution to task 1

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet code - use strace to find out why apache is returning a 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
